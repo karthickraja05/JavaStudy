2390a31d9774f7da32137456b83f6cf6364194ac
karthickraja2303 <karthickraja.n@thewholewave.com>
2026-03-18 18:22:04 +0530

    code updated

--- awt/Form.java
+++ awt/Form.java
@@ -7,52 +7,33 @@ import java.util.TimerTask;
 import util.DataSave;
 
 public class Form {
-    
-    public static void main(String[] args) {
-        DataSave dataSave = new DataSave();
-        
-        Frame frame = new Frame("Form Submit");
+    DataSave file;
+    Label email;
+    Label password;
+    Label age;
+    Label message;
+    TextField emailInput;
+    TextField passwordInput;
+    TextField ageInput;
+    Frame frame;
+    Button btn;
 
-        Label email = new Label("Email");
-        email.setBounds(50,100,100,30);
-        frame.add(email);
+    Form(DataSave file){
+        this.file = file;
+        this.frame = new Frame();
+    }
 
-        TextField emailInput = new TextField("Enter your email");
-        emailInput.setBounds(170,100,200 ,30);
-        frame.add(emailInput);
+    public void render(){
+        addWidgets();
+        listen();
+    }
 
-        Label password = new Label("Password");
-        password.setBounds(50,150,100,30);
-        frame.add(password);
-
-        TextField passwordInput = new TextField("Enter your password");
-        passwordInput.setBounds(170,150,200 ,30);
-        frame.add(passwordInput);
-
-        Label age = new Label("Age");
-        age.setBounds(50,200,100,30);
-        frame.add(age);
-
-        TextField ageInput = new TextField("Age");
-        ageInput.setBounds(170,200,200 ,30);
-        frame.add(ageInput);
-
-        Button btn = new Button("Submit");
-        btn.setBounds(170,250,100,30);
-        frame.add(btn);
-
-        Label message = new Label("");
-        message.setBounds(170,300,200,30);
-        frame.add(message);
-
-        ActionListener obje = new ActionListener() {
+    public void listen(){
+         ActionListener obje = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println(emailInput.getText());
-                System.out.println(passwordInput.getText());
-                System.out.println(ageInput.getText());
                 message.setText("Data saved successfully!");
                 removeMessage(message);
-                dataSave.save(emailInput.getText(),passwordInput.getText(),ageInput.getText());
+                file.save(emailInput.getText(),passwordInput.getText(),ageInput.getText());
                 emailInput.setText("");
                 passwordInput.setText("");
                 ageInput.setText("");
@@ -60,13 +41,50 @@ public class Form {
         };
 
         btn.addActionListener(obje);
-        
+    }
+
+    public void addWidgets(){
+        email = new Label("Email");
+        email.setBounds(50,100,100,30);
+        frame.add(email);
+
+        emailInput = new TextField("Enter your email");
+        emailInput.setBounds(170,100,200 ,30);
+        frame.add(emailInput);
+
+        password = new Label("Password");
+        password.setBounds(50,150,100,30);
+        frame.add(password);
+
+        passwordInput = new TextField("Enter your password");
+        passwordInput.setBounds(170,150,200 ,30);
+        frame.add(passwordInput);
+
+        age = new Label("Age");
+        age.setBounds(50,200,100,30);
+        frame.add(age);
+
+        ageInput = new TextField("Age");
+        ageInput.setBounds(170,200,200 ,30);
+        frame.add(ageInput);
+
+        btn = new Button("Submit");
+        btn.setBounds(170,250,100,30);
+        frame.add(btn);
+
+        message = new Label("");
+        message.setBounds(170,300,200,30);
+        frame.add(message);
+
         frame.setLayout(null);
         frame.setSize(500,500);
         frame.setVisible(true);
+    }
 
-        
-        
+    public static void main(String[] args) {
+        DataSave dataSave = new DataSave();
+        Form form = new Form(dataSave);
+        form.render();
     }
 
     public static void removeMessage(Label message){
